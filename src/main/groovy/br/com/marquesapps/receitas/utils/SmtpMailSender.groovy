54ef847813685b337b4e05f6b082ad9b70a0caa1
Luiz Marques <liunit@gmail.com>
2015-10-04 07:23:14 -0300

    Email

--- src/main/groovy/br/com/marquesapps/receitas/utils/SmtpMailSender.groovy
+++ src/main/groovy/br/com/marquesapps/receitas/utils/SmtpMailSender.groovy
@@ -3,27 +3,41 @@ package br.com.marquesapps.receitas.utils;
 import javax.mail.MessagingException
 import javax.mail.internet.MimeMessage
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.mail.javamail.JavaMailSenderImpl
 import org.springframework.mail.javamail.MimeMessageHelper
 import org.springframework.stereotype.Component
 
-import br.com.marquesapps.receitas.config.Mail
-
 @Component
 public class SmtpMailSender {
+	
+	@Value('${spring.mail.username}')
+	private String username;
+	@Value('${spring.mail.password}')
+	private String password;
+	@Value('${spring.mail.host}')
+	private String host;
+	@Value('${spring.mail.port}')
+	private int port;
+	@Value('${mail.smtp.starttls.enable}')
+	private String starttlsenable;
+	@Value('${mail.smtp.starttls.required}')
+	private String starttlsrequired;
+	@Value('${mail.smtp.auth}')
+	private String smtpauth;
 	
 	def send(String to, String subject, String body) throws MessagingException {
 		
 		// of course you would use DI in any real-world cases
 		JavaMailSenderImpl sender = new JavaMailSenderImpl();
 		Properties javaMailProperties = new Properties();		
-		sender.setHost(Mail.getHost());
-		sender.setUsername(Mail.getUsername());
-	    sender.setPassword(Mail.getPassword());
-		sender.setPort(Mail.getPort());		
-		javaMailProperties.setProperty("mail.smtp.auth",  Mail.getSmtpauth());
-		javaMailProperties.setProperty("mail.smtp.starttls.enable", Mail.getStarttlsenable());
-		javaMailProperties.setProperty("mail.smtp.starttls.required", Mail.getStarttlsrequired());
+		sender.setHost(this.host);
+		sender.setUsername(this.username);
+	    sender.setPassword(this.password);
+		sender.setPort(this.port);		
+		javaMailProperties.setProperty("mail.smtp.auth",  this.smtpauth);
+		javaMailProperties.setProperty("mail.smtp.starttls.enable", this.starttlsenable);
+		javaMailProperties.setProperty("mail.smtp.starttls.required", this.starttlsrequired);
 		sender.setJavaMailProperties(javaMailProperties);
 		
 		MimeMessage message = sender.createMimeMessage();
